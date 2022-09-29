66432ca99a42ca6ea162f69391bbca568c08e1d3
Philip Olesen <nb.philip@gmail.com>
2022-09-29 18:35:31 +0200

    comments+nextline idiocy fix

--- Calculator.java
+++ Calculator.java
@@ -2,25 +2,28 @@ import java.util.Scanner;
 
 public class Calculator {
   public static void main(String[] args) {
-    Boolean bool = true;
+    Boolean loopProgram = true;
+    // try for cleaner code, but no error handling
     try (Scanner scan = new Scanner(System.in)) {
-      while (bool) {
-        System.out.println("Press 1 for addition, 2 for subtraction,\n3 for multiplication, 4 for division.\n5 to exit.");
-        switch(scan.nextInt()) {
-          case 1:
+      while (loopProgram) {
+        System.out.println("Enter 1 for addition, 2 for subtraction,\n3 for multiplication, 4 for division.\n5 to exit.");
+        // switch-case to handle selection
+        switch(scan.next()) {
+          case "1":
             Addition(scan);
             break;
-          case 2:
+          case "2":
             Subtraction(scan);
             break;
-          case 3:
+          case "3":
             Multiplication(scan);
             break;
-          case 4:
+          case "4":
             Division(scan);
             break;
-          case 5:
-            bool = false;
+          // case 5 ends program by setting bool = false. Cannot use break due to being inside switch-case
+          case "5":
+            loopProgram = false;
             break;
           default:
             System.out.println("Selection failed");
@@ -29,27 +32,32 @@ public class Calculator {
       }
     }
   }
-  private static int[] GetNumbers(Scanner scan) {
-    int num[]  = new int[2];
+  // method for retrieving numbers as double in array
+  private static double[] GetNumbers(Scanner scan) {
+    double num[]  = new double[2];
     System.out.println("Enter first number, then the second.");
     num[0] = scan.nextInt();
     num[1] = scan.nextInt();
     return num;
   }
+  // method for addition and printing it
   private static void Addition(Scanner scan) {
-    int num[] = GetNumbers(scan);
+    double num[] = GetNumbers(scan);
     System.out.println(num[0] + num[1]);
   }
+  // method for subtraction and printing it
   private static void Subtraction(Scanner scan) {
-    int num[] = GetNumbers(scan);
+    double num[] = GetNumbers(scan);
     System.out.println(num[0] - num[1]);
   }
+  // method for multiplication and printing it
   private static void Multiplication(Scanner scan) {
-    int num[] = GetNumbers(scan);
+    double num[] = GetNumbers(scan);
     System.out.println(num[0] * num[1]);
   }
+  // method for division and printing it
   private static void Division(Scanner scan) {
-    int num[] = GetNumbers(scan);
+    double num[] = GetNumbers(scan);
     System.out.println(num[0] / num[1]);
   }
 }
